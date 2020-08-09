932d7f2fa28c64eb381d611da8cad5e3948d344f
abdulkatheer <MunbeVaa#1037>
2020-08-09 11:04:06 +0530

    Rename application name

--- src/test/java/io/abdul/springbootdemo/EmployeeManagementApplicationTest.java
+++ src/test/java/io/abdul/springbootdemo/EmployeeManagementApplicationTest.java
@@ -1,13 +1,16 @@
 package io.abdul.springbootdemo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringBootDemoApplicationTests {
+@Slf4j
+class EmployeeManagementApplicationTest {
 
     @Test
     void contextLoads() {
+        log.info("Spring Context loaded successfully!");
     }
 
 }
